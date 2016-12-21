0613be5cbfc1724d9824b447d464c91690c7bad6
StephaneC <Stephane.castrec@gmail.com>
2016-12-21 13:34:56 +0100

    adding CORS

--- src/main/java/com.brestopencampus.spark/Application.java
+++ src/main/java/com.brestopencampus.spark/Application.java
@@ -21,6 +21,8 @@ public class Application {
     exception(Exception.class, (e, req, res) -> e.printStackTrace()); // print all exceptions
     port(8080);
 
+    enableCORS("*", "*", "*");
+
     BeerDao dao = new BeerDao(mongo());
 
     get("/", (req, res) -> "Hello Beers");
@@ -39,6 +41,33 @@ public class Application {
       return b;
     }, jsonT);
     delete("/json/Beers/:id", (ICRoute) (req) -> dao.remove(req.params("id")));
+  }
+
+  // Enables CORS on requests. This method is an initialization method and should be called once.
+  private static void enableCORS(final String origin, final String methods, final String headers) {
+
+    options("/*", (request, response) -> {
+
+      String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+      if (accessControlRequestHeaders != null) {
+        response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+      }
+
+      String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+      if (accessControlRequestMethod != null) {
+        response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+      }
+
+      return "OK";
+    });
+
+    before((request, response) -> {
+      response.header("Access-Control-Allow-Origin", origin);
+      response.header("Access-Control-Request-Method", methods);
+      response.header("Access-Control-Allow-Headers", headers);
+      // Note: this may or may not be necessary in your particular application
+      response.type("application/json");
+    });
   }
 
   private static DB mongo() throws Exception {
